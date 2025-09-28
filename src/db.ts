183117c3cc2a1cb45ab68ac11e748c254a0cfd41
Henrik Suurik <henrik.suurik@chasacademy.se>
2025-09-28 19:57:33 +0200

    Ensure Mongoose Indexes Are Created

--- src/db.ts
+++ src/db.ts
@@ -2,10 +2,17 @@
 
 import mongoose from "mongoose";
 import config from "./config.js";
+import { User, Project, Task } from "./models";
 
 export async function connectDB() {
     if (!config.dbUri) throw new Error("Missing MONGODB_URI env variable");
     await mongoose.connect(config.dbUri);
+    // Ensure Mongoose Indexes Are Created
+    await Promise.all([
+      User.init(),
+      Project.init(),
+      Task.init(),
+    ]);
     console.log("MongoDB connected");
 }
 
