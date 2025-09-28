1e2fae830f8d6560fa10bf5419b6cd3d32aeb78a
Henrik Suurik <henrik.suurik@chasacademy.se>
2025-09-28 10:35:31 +0200

    Removed generics.ts

--- src/types/generic.ts
+++
@@ -1,5 +0,0 @@
-// src/types/generic.ts
-
-type GenericApiOK = { ok: true; message: string; data?: object | undefined };
-type GenericApiErr = { ok: false; message: string; errors?: object[] | undefined };
-export type GenericApiResponse = GenericApiOK | GenericApiErr;
