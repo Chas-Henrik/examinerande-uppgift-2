f63c92f716a6c499b7d18e404a5d4c19835f163a
Henrik Suurik <henrik.suurik@chasacademy.se>
2025-09-29 09:30:09 +0200

    Renamed async.ts to asyncHandler.ts

---
+++ src/utils/asyncHandler.ts
@@ -0,0 +1,9 @@
+// src/utils/async.ts
+
+import { Request, Response, NextFunction, RequestHandler } from 'express';
+
+export function asyncHandler(fn: RequestHandler) {
+    return (req: Request, res: Response, next: NextFunction) => {
+        Promise.resolve(fn(req, res, next)).catch(next);    // Pass errors to the next middleware (error handler)
+    }
+}
